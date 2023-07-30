de472ded4c0c3158e7366a8b951ea56280e56141
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-07-30 20:19:27 +0900

    feat: repository용 테스트 어노테이션 추가

---
+++ src/test/java/com/my/authserver/annotation/MyDataJpaTest.java
@@ -0,0 +1,18 @@
+package com.my.authserver.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import com.my.authserver.config.querydsl.QueryDslConfig;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@DataJpaTest
+@Import(QueryDslConfig.class)
+public @interface MyDataJpaTest {
+}
