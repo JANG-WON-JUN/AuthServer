8889230d2cee773e4e73c811b9c4eef429c51399
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-07-30 23:58:26 +0900

    feature: findByRoleName 메소드의 반환타입을 optional로 변경

--- src/main/java/com/my/authserver/member/auth/repository/RoleRepository.java
+++ src/main/java/com/my/authserver/member/auth/repository/RoleRepository.java
@@ -1,11 +1,13 @@
 package com.my.authserver.member.auth.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.my.authserver.domain.entity.member.auth.Role;
-import com.my.authserver.member.enums.Roles;
+import com.my.authserver.member.enums.RoleType;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-	Role findByRoleName(Roles role);
+	Optional<Role> findByRoleType(RoleType roleType);
 }
