d19c27a7e8ea4115a157553f83b3884d82916337
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-07-31 00:06:42 +0900

    feature: deleteRole 메소드 추가

--- src/main/java/com/my/authserver/member/auth/service/RoleService.java
+++ src/main/java/com/my/authserver/member/auth/service/RoleService.java
@@ -2,9 +2,12 @@ package com.my.authserver.member.auth.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
+import com.my.authserver.common.utils.MessageSourceUtils;
 import com.my.authserver.domain.entity.member.auth.Role;
 import com.my.authserver.member.auth.repository.RoleRepository;
+import com.my.authserver.member.auth.service.request.RoleCreateServiceRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,10 +16,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RoleService {
 
+	private final MessageSourceUtils messageSourceUtils;
+	private final RoleQueryService roleQueryService;
 	private final RoleRepository roleRepository;
 
-	public void createRole(Role role) {
-		roleRepository.save(role);
+	public Long createRole(RoleCreateServiceRequest request) {
+		Assert.notNull(request.getRoleType(), messageSourceUtils.getMessage("error.noRoleType"));
+		Assert.hasText(request.getRoleDesc(), messageSourceUtils.getMessage("error.noRoleDesc"));
+
+		Role savedRole = roleRepository.save(request.toEntity());
+
+		return savedRole.getId();
+	}
+
+	public void deleteRole(Long id) {
+		Long savedRoleId = roleQueryService.findById(id).getId();
+		roleRepository.deleteById(savedRoleId);
 	}
 
 }
