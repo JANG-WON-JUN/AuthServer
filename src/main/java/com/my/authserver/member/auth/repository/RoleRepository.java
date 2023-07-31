937e55b92df912410cf264b89544e204a4e329a5
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-08-01 00:06:10 +0900

    hotfix: 누락된 레포지토리 추가

--- src/main/java/com/my/authserver/member/auth/repository/RoleRepository.java
+++ src/main/java/com/my/authserver/member/auth/repository/RoleRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.my.authserver.domain.entity.member.auth.Role;
 import com.my.authserver.member.enums.RoleType;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long>, RoleQueryRepository {
 
 	Optional<Role> findByRoleType(RoleType roleType);
 }
