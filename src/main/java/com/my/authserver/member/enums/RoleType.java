53a5b83aac5a6668f13402e89f037d958eb66c95
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-08-01 10:00:25 +0900

    feature: hasHigherPriorityThan 메소드 추가

--- src/main/java/com/my/authserver/member/enums/RoleType.java
+++ src/main/java/com/my/authserver/member/enums/RoleType.java
@@ -10,16 +10,21 @@ import lombok.Getter;
 public enum RoleType {
 	// Spring Security에서는 권한을 나타내는 문자열의 접두어에 기본으로 ROLE_를 붙여주므로
 	// 동일하게 Roles enum을 작성함
-	ROLE_ANONYMOUS("비회원"),
-	ROLE_MEMBER("회원"),
-	ROLE_ADMIN("관리자");
+	ROLE_ANONYMOUS("비회원", 0),
+	ROLE_MEMBER("회원", 1),
+	ROLE_ADMIN("관리자", 2);
 
 	private String roleDesc;
+	private int priority;
 
 	public static RoleType findRole(String roleName) {
 		return Arrays.stream(RoleType.values())
 			.filter(role -> role.name().equals(roleName))
 			.findFirst()
 			.orElse(ROLE_ANONYMOUS);
+	}
+
+	public boolean hasHigherPriorityThan(RoleType roleType) {
+		return priority > roleType.getPriority();
 	}
 }
