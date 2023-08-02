ff4bc9a98b3009ee47e9c6f2ce9173ac36f11bb8
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-08-03 01:36:26 +0900

    feature: 비밀번호 CRU 기능 추가 완료

--- src/main/java/com/my/authserver/member/service/PasswordService.java
+++ src/main/java/com/my/authserver/member/service/PasswordService.java
@@ -2,9 +2,15 @@ package com.my.authserver.member.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
+import com.my.authserver.common.utils.MessageSourceUtils;
+import com.my.authserver.common.web.exception.PasswordNotValid;
 import com.my.authserver.domain.entity.member.Password;
 import com.my.authserver.member.repository.PasswordRepository;
+import com.my.authserver.member.service.query.PasswordQueryService;
+import com.my.authserver.member.service.request.PasswordCreateServiceRequest;
+import com.my.authserver.member.service.request.PasswordUpdateServiceRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,10 +19,37 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PasswordService {
 
+	private final PasswordQueryService passwordQueryService;
+	private final MessageSourceUtils messageSourceUtils;
 	private final PasswordRepository passwordRepository;
 
-	public void createPassword(Password password) {
-		password.extendBeChangedDate();
-		passwordRepository.save(password);
+	public Long createPassword(PasswordCreateServiceRequest request) {
+		Assert.hasText(request.getPassword(), messageSourceUtils.getMessage("field.required.password"));
+		Assert.notNull(request.getLastModDateTime(), messageSourceUtils.getMessage("field.required.lastModDateTime"));
+		Assert.notNull(request.getLockLimitMinutes(), messageSourceUtils.getMessage("field.required.lockLimitMinutes"));
+		Assert.notNull(request.getChangeCycle(), messageSourceUtils.getMessage("field.required.changeCycle"));
+
+		Password password = request.toEntity();
+		Password savedPassword = passwordRepository.save(password);
+
+		return savedPassword.getId();
+	}
+
+	public Long updatePassword(PasswordUpdateServiceRequest request) {
+		Assert.hasText(request.getPassword(), messageSourceUtils.getMessage("field.required.password"));
+
+		if (notMatched(request.getPassword(), request.getPasswordConfirm())) {
+			throw new PasswordNotValid(messageSourceUtils.getMessage("error.notValidPassword"));
+		}
+		
+		Password savedPassword = passwordQueryService.findById(request.getId());
+
+		savedPassword.changePassword(request.getPassword());
+
+		return savedPassword.getId();
+	}
+
+	private boolean notMatched(String password, String passwordConfirm) {
+		return !password.equals(passwordConfirm);
 	}
 }
